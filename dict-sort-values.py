a28c95a3d9bd40ae77a05032fafc1faf3d77da5b
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-03-01 18:56:45 +0200

    added a for loop to create new dictionary

--- dict-sort-values.py
+++ dict-sort-values.py
@@ -7,3 +7,15 @@ while name != "stop":
 
 print (dict1)    
 
+new_dict ={}
+
+for i in dict1:
+    age_value= dict1[i]
+    if age_value in new_dict:
+        new_dict[age_value].append(i)
+    else:
+        new_dict[age_value]= [i]
+    
+
+print(new_dict)
+
